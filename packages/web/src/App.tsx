16432f715637fe56159e3c5d6af4d38ef9f6876d
DmytroOvchar <ov4ar.dmytro@gmail.com>
2023-08-16 22:46:44 +0300

    set Up tailwind

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,35 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        {/* NEW */}
-        <button
-          onClick={() => {
-            fetch("http://localhost:3001", {})
-              .then((response) => response.json())
-              .then((data) => console.log(data));
-          }}
-        >
-          GET SOME DATA!!!
-        </button>
-      </header>
+      {/* NEW */}
+      <button
+        onClick={() => {
+          fetch("http://localhost:3001", {})
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+        }}
+      >
+        GET SOME DATA!!!
+      </button>
+      {/* tailwind */}
+      <h1 className="text-3xl font-bold underline text-my-red">Hello world!</h1>
     </div>
   );
 }
