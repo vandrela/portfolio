5ad0005eb1e917bdf4865efb1376b5784caff100
Il1aMarkulchak <ilya.markulchak@mail.ru>
2023-09-30 14:08:21 +0100

    Added mongodb

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,11 +1,34 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
 const port = 3001;
+const url = 'mongodb+srv://IliaMarkulcsak:02062004_Iliya@cluster0.elkgw12.mongodb.net/?retryWrites=true&w=majority';
+function connect() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield mongoose_1.default.connect(url);
+            console.log('Connected to the database');
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+}
+connect();
 // allow CORS for all routes
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
