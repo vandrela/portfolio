fadd09e9ae48d7582e1d76058af2913cf5875022
vandrela <andrela.vasya@gmail.com>
2023-11-13 15:55:28 +0000

    all fixed, all working

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -3,21 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-<<<<<<< HEAD
-=======
-      screens: {
-        "2xl": { max: "1280px" },
-        xl: { max: "1024px" },
-        lg: { max: "992px" },
-        md: { max: "768px" },
-        xs: { max: "480px" },
-      },
->>>>>>> 47bb8595c0a03711aa3411080bc8874dc20534f4
       colors: {
         "my-red": "#fc1100",
       },
     },
-<<<<<<< HEAD
     screens: {
       "2xl": { max: "1280px" },
       xl: { max: "1024px" },
@@ -26,8 +15,6 @@ module.exports = {
       sm: { max: "768px" },
       xs: { max: "480px" },
     },
-=======
->>>>>>> 47bb8595c0a03711aa3411080bc8874dc20534f4
   },
   plugins: [],
 };
