2cd80ec35cdeb415057da0f31b24d2e153422903
DmytroOvchar <ov4ar.dmytro@gmail.com>
2024-01-09 00:46:08 +0200

    added form component, installed react-hook-form library

--- packages/web/src/components/molecules/OurValuesBlock/index.tsx
+++ packages/web/src/components/molecules/OurValuesBlock/index.tsx
@@ -1,15 +1,24 @@
 import { CarouselProps } from "../../../types/carouselTypes";
 import { TitleBlock } from "../../atoms/TitleBlock";
 import { Carousel } from "../Carousel";
+import { useMediaQuery } from "@react-hook/media-query";
 
 export const OurValuesBlock = ({ slides }: CarouselProps) => {
+  const isSmallScreen = useMediaQuery("(max-width: 710px)");
+  const gradientRotationDegree = !isSmallScreen
+    ? "bg-[linear-gradient(180deg,#00360C_0%,#280036_100%)]"
+    : "bg-[linear-gradient(0deg,#00360C_0%,#280036_100%)]";
+
   return (
-    <>
+    <div className="relative z-0">
       <TitleBlock
         name="Our values"
         classNames="text-center mb-[clamp(40px,5vw,80px)]"
       />
+      <div
+        className={`transition-all -top-[-40] absolute w-[100%] h-[150%]  blur-[175px] z-[-1] opacity-50 ${gradientRotationDegree}`}
+      />
       <Carousel slides={slides} />
-    </>
+    </div>
   );
 };
