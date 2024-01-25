5aa791436e0fbbbd2ee493b9528f724a545927fb
Pavlo Bodo <pavlo.bodjo@gmail.com>
2024-01-25 20:18:04 +0000

    added wrapper around a cart

--- packages/web/src/components/molecules/OurProfilers/index.tsx
+++ packages/web/src/components/molecules/OurProfilers/index.tsx
@@ -12,30 +12,30 @@ export const OurProfilers = () => {
     <div className="relative z-1">
       <TitleBlock classNames="text-center text-white" name="Our profilers" />
       <div className="w-full h-auto flex gap-[75px] flex justify-center items-center pt-20 mb-[347px]">
-        <div
-          className="w-[375px] h-auto bg-[#171927] flex flex-col gap-10 p-4 rounded-[20px]"
-          ref={ref}
-        >
-          <img src="/images/profile_image.png" alt="some photo" />
+        <div className="hover:border hover:border-gray-600 hover:bg-white03 rounded-[20px] p-4">
+          <div className="w-[375px] h-auto  flex flex-col gap-10  " ref={ref}>
+            <img
+              src="/images/profile_image.png"
+              alt="some photo"
+              className="object-cover"
+            />
 
-          <div className="flex flex-col items-center ">
-            <h3 className="text-white text-center text-2xl font-semibold mb-5 ">
-              Tobias Williams
-            </h3>
-            <span className="text-white text-xl font-base border-t border-white20 pt-3 ">
-              Frontend developer
-            </span>
+            <div className="flex flex-col items-center ">
+              <h3 className="text-white text-center text-2xl font-semibold mb-5 ">
+                Tobias Williams
+              </h3>
+              <span className="text-white text-xl font-base border-t border-white20 pt-3 ">
+                Frontend developer
+              </span>
+            </div>
+            <Button
+              label="View profile"
+              classNames={`px-10 py-4 text-white font-semibold text-[20px] self-center  ${
+                hovering ? "scale-x-150 " : ""
+              }  `}
+            />
           </div>
-          <Button
-            label="View profile"
-            classNames={`px-10 py-4 text-white font-semibold text-[20px] self-center  ${
-              hovering ? "scale-x-150 " : ""
-            }  `}
-          />
         </div>
-        <div className="w-[375px] h-[705px] bg-[#7e22ce]">2</div>
-        <div className="w-[375px] h-[705px] bg-[#7e22ce]">3</div>
-        <div className="w-[375px] h-[705px] bg-[#7e22ce]">4</div>
       </div>
     </div>
   );
