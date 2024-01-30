f753bd3871de0eba99202e1b7a73639c990e4318
Pavlo Bodo <pavlo.bodjo@gmail.com>
2024-01-30 19:49:52 +0000

    we add smooth scroll

--- packages/web/src/components/molecules/ThankYouAkaFooter/index.tsx
+++ packages/web/src/components/molecules/ThankYouAkaFooter/index.tsx
@@ -3,8 +3,11 @@ import { Button } from "../../atoms/Button";
 import { TitleBlock } from "../../atoms/TitleBlock";
 
 function scrollTopFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
 }
 
 const violetColor =
